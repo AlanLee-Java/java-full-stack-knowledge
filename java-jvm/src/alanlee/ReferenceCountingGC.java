8242c94c9855fd87522a569806e9502fb268bc87
AlanLee <19865879289@163.com>
2023-01-08 23:42:21 +0800

    循环引用

---
+++ java-jvm/src/alanlee/ReferenceCountingGC.java
@@ -0,0 +1,14 @@
+package alanlee;
+
+public class ReferenceCountingGC {
+
+    public Object instance = null;
+
+    public static void main(String[] args) {
+        ReferenceCountingGC objectA = new ReferenceCountingGC();
+        ReferenceCountingGC objectB = new ReferenceCountingGC();
+        objectA.instance = objectB;
+        objectB.instance = objectA;
+    }
+
+}
