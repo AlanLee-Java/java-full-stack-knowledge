b46242b7624ff35e83f6b951de8d0df262ee45ff
saimi <saimi@janezt.com>
2023-08-18 18:21:10 +0800

    Exception

---
+++ java-based/src/alanlee/ExceptionTest.java
@@ -0,0 +1,45 @@
+package alanlee;
+
+public class ExceptionTest {
+
+    private int testTimes;
+
+    public ExceptionTest(int testTimes) {
+        this.testTimes = testTimes;
+    }
+
+    public void newObject() {
+        long l = System.nanoTime();
+        for (int i = 0; i < testTimes; i++) {
+            new Object();
+        }
+        System.out.println("建立对象：" + (System.nanoTime() - l));
+    }
+
+    public void newException() {
+        long l = System.nanoTime();
+        for (int i = 0; i < testTimes; i++) {
+            new Exception();
+        }
+        System.out.println("建立异常对象：" + (System.nanoTime() - l));
+    }
+
+    public void catchException() {
+        long l = System.nanoTime();
+        for (int i = 0; i < testTimes; i++) {
+            try {
+                throw new Exception();
+            } catch (Exception e) {
+            }
+        }
+        System.out.println("建立、抛出并接住异常对象：" + (System.nanoTime() - l));
+    }
+
+    public static void main(String[] args) {
+        ExceptionTest test = new ExceptionTest(10000);
+        test.newObject();
+        test.newException();
+        test.catchException();
+    }
+
+}
