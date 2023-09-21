dd515949523d004f099c9c1e25be5a592c48f342
saimi <saimi@janezt.com>
2023-09-21 10:39:41 +0800

    线程同步

--- java-multithreading/src/alanlee/SynchronizedExample.java
+++ java-multithreading/src/alanlee/SynchronizedExample.java
@@ -13,7 +13,7 @@ public class SynchronizedExample {
 
     public void func1() {
         synchronized (this) {
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < 100; i++) {
                 System.out.print(i + " ");
             }
         }
--- java-multithreading/src/alanlee/SynchronizedExample1.java
+++ java-multithreading/src/alanlee/SynchronizedExample1.java
@@ -13,7 +13,7 @@ public class SynchronizedExample1 {
 
     public void func2() {
         synchronized (SynchronizedExample1.class) {
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < 100; i++) {
                 System.out.print(i + " ");
             }
         }
