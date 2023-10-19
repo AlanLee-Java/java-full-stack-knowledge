7bd3ddfce0d6473ac0b01b0b3377a46b05f0eb2d
saimi <saimi@janezt.com>
2023-10-19 16:39:43 +0800

    String

---
+++ java-based/src/alanlee/TestStringAddNull.java
@@ -0,0 +1,17 @@
+package alanlee;
+
+/**
+ * 测试字符串+null
+ *
+ * @author saimi
+ * @date 2023-10-19
+ */
+public class TestStringAddNull {
+
+    public static void main(String[] args) {
+        String a = "a";
+        String b = null;
+        System.out.println(a + b);
+    }
+
+}
