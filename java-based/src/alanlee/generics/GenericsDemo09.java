23c42e06c42facb62fb68c7ffbabec910e3c41b3
saimi <saimi@janezt.com>
2024-08-08 16:03:22 +0800

    Generics

--- java-based/src/alanlee/generics/GenericsDemo09.java
+++ java-based/src/alanlee/generics/GenericsDemo09.java
@@ -13,8 +13,8 @@ public class GenericsDemo09 {
         t = new Notepad<String, Integer>();       // 里面的key为String，value为Integer
         t.setKey("汤姆");        // 设置第一个内容
         t.setValue(20);            // 设置第二个内容
-        System.out.print("姓名；" + t.getKey());      // 取得信息
-        System.out.print("，年龄；" + t.getValue());       // 取得信息
+        System.out.print("姓名：" + t.getKey());      // 取得信息
+        System.out.print("，年龄：" + t.getValue());       // 取得信息
     }
 
 }
